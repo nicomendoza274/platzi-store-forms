7a3cfcf7890470a8c5b8e89c9562015d2d8bd0f2
Nico Mendoza <nicomendoza274@gmail.com>
2023-02-26 23:15:29 -0300

    auto-update service worker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   template: '<router-outlet></router-outlet>',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private swUpdate: SwUpdate) {
+
+  }
+  ngOnInit(): void {
+    this.updatePWA();
+  }
+
+  updatePWA() {
+    this.swUpdate.available.subscribe(value => {
+      console.log('update:', value)
+      window.location.reload()
+    })
+  }
 }
